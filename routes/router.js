1c3ef4ef89371044666b1b586b0fff41856cbe0a
KdavidRocha10 <kevinrocha239@gmail.com>
2022-11-30 22:15:37 -0500

    cambios

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,5 @@
-const express = require("express");
-const router = express.Router();
-const conexion = require("../database/db");
-const crud = require("../controllers/crud");
+// const express = require("express");
+// const router = express.Router();
 
 //constante para definir el controlador
 const authController = require("../controllers/authController");
@@ -18,65 +16,10 @@ router.get("/index", (req, res) => {
 router.get("/login", (req, res) => {
   res.render("login", { alert: false });
 });
-router.get("/register", (req, res) => {
-  res.render("register");
-});
-
-router.get("/option", (req, res) => {
-  res.render("option", { alert: false });
-});
-
-router.get("/registro_jug", (req, res) => {
-  // res.render("registro_jug");
-
-  //Plantilla adminser -->tecnicos de usuario
-  conexion.query("SELECT * FROM categoria", (error, result) => {
-    if (error) {
-      throw error;
-    } else {
-      res.render("registro_jug", { result: result });
-    }
-  });
-});
-
-router.get("/mensualidad", (req, res) => {
-  // res.render("registro_jug");
-  res.render("mensualidad");
-});
-
-router.get("/adminuser", (req, res) => {
-  //Plantilla adminser -->tecnicos de usuario
-  conexion.query(
-    "SELECT alumno.id_alumno, alumno.nombre, alumno.apellido, alumno.fecha_nacimiento, alumno.numid,alumno.eps, alumno.acudiente, alumno.tel_acudiente , categoria.descripcion FROM alumno inner join categoria on alumno.id_categoria=categoria.id_categoria",
-    (error, result) => {
-      if (error) {
-        throw error;
-      } else {
-        res.render("adminuser", { result: result });
-      }
-    }
-  );
-});
-
-router.get("/busquedas", (req, res) => {
-  //Plantilla adminser -->tecnicos de usuario
-  conexion.query(
-    "SELECT categoria.descripcion, alumno.nombre, alumno.apellido FROM categoria inner join alumno on  alumno.id_categoria=categoria.id_categoria;",
-    (error, result) => {
-      if (error) {
-        throw error;
-      } else {
-        res.render("busquedas", { result: result });
-      }
-    }
-  );
-});
 
 //router para los métodos del controller
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
 
-//Metodos CRUD para el registro de tareas--------------
-router.post("/addPersona", crud.addPersona);
 module.exports = router;
