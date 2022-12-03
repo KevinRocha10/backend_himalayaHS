a2a9fb09e7aaa61d96621ac86cc7ed624ded2e0b
FelipeLL <f3lip3.3z@hotmail.com>
2022-12-03 10:04:02 -0500

    Se agrego migracion, modelo y seeder de usuarios

--- seeders/20221203145702-demo-user.js
+++ seeders/20221203145702-demo-user.js
@@ -4,13 +4,19 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
 
-    await queryInterface.bulkInsert('Roles', [{
-      name: 'Admin',
+    await queryInterface.bulkInsert('Users', [{
+      name: 'Administrador',
+      password: "1234",
+      email: "Admin@gmail.com",
+      id_tipo: 1,
       createdAt: new Date,
       updatedAt: new Date,
     },
     {
-      name: 'Standard',
+      name: 'John Doe',
+      password: "1234",
+      email: "John@gmail.com",
+      id_tipo: 2,
       createdAt: new Date,
       updatedAt: new Date,
     }
